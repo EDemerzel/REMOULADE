d6c9e0ee8acc27af37773d9cab6a5273f8f23492
Arthur Choung <arthur_choung@me.com>
2021-08-03 17:09:50 +1000

    Simple script to open an attachment

---
+++ remoulade-open.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+$path = shift @ARGV;
+$part = shift @ARGV;
+$mimeType = shift @ARGV;
+
+if (not $path or not $part or not $mimeType) {
+    die('no path, part or mimeType');
+}
+
+$str = "path $path part $part mimeType $mimeType";
+if ($mimeType eq 'text/html') {
+    system("remoulade-extractPart $path $part >/tmp/remoulade.html");
+    system("chromium /tmp/remoulade.html");
+} elsif ($mimeType eq 'application/pdf') {
+    system("remoulade-extractPart $path $part >/tmp/remoulade.pdf");
+    system("mupdf /tmp/remoulade.pdf");
+} elsif ($mimeType eq 'image/png') {
+    system("remoulade-extractPart $path $part >/tmp/remoulade.png");
+    system("chromium /tmp/remoulade.png");
+} else {
+    system("echo $path $part $mimeType | hotdog alert");
+}
