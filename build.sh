0e46c36cc8599b7ea27e632dc21e0572265ed52e
Arthur Choung <arthur_choung@me.com>
2021-08-03 16:53:39 +1000

    Updated build script

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 set -x
 gcc -o remoulade remoulade.c
 
+gcc -o remoulade-list `pkg-config --cflags --libs gmime-3.0` remoulade-list.c 
+gcc -o remoulade-extractPart `pkg-config --cflags --libs gmime-3.0` remoulade-extractPart.c 
+gcc -o remoulade-printMessage `pkg-config --cflags --libs gmime-3.0` remoulade-printMessage.c 
+
